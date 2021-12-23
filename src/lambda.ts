35d7a009405e288c61672eb4cabb3fabd16419c9
roleyfoley <roleyfoley@gmail.com>
2021-12-23 11:44:18 +1100

    fix: lambda entry

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,7 +1,7 @@
 import serverless from 'serverless-http';
 import 'dotenv/config';
 
-import App from 'app';
+import App from './app';
 
 import { HealthCheckController } from 'health-check/health-check.controller';
 import { VerifyController } from 'verify/verify.controller';
