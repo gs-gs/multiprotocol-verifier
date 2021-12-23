3c38d9ec2ce67813441116ecf2c98abc486dc899
roleyfoley <roleyfoley@gmail.com>
2021-12-23 11:51:33 +1100

    fix: relative src for funcitons

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 
 import App from './app';
 
-import { HealthCheckController } from 'health-check/health-check.controller';
-import { VerifyController } from 'verify/verify.controller';
+import { HealthCheckController } from './health-check/health-check.controller';
+import { VerifyController } from './verify/verify.controller';
 
 const app = new App([new HealthCheckController(), new VerifyController()]);
 
